942231973595172ae4fa6c926c4316cf6e992197
TauqeerDevX <hasantauqeer20@gmail.com>
2025-02-16 18:20:10 +0530

    Styling Through external css

---
+++ Styling/src/App.jsx
@@ -0,0 +1,19 @@
+// How to style react component with css
+// not including external frameworks or preprocessors
+
+// 1. External
+// 2. Modules
+// 3. Inline
+
+import Button from "./Button";
+
+function App() {
+
+  return (
+    <>
+    <Button/>
+    </>
+  )
+}
+
+export default App
