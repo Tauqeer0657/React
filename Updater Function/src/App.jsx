e4140f809b24c327781078106b83cc7a36f728c3
TauqeerDevX <hasantauqeer20@gmail.com>
2025-02-19 00:25:47 +0530

    Updater Function

---
+++ Updater Function/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Updater from './Updater'
+function App() {
+
+  return (
+    <>
+    <Updater/>
+    </>
+  )
+}
+
+export default App
---
+++ Updater Function/src/Updater.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increaseCount = () => {
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(count + 1);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+
+    }
+    const resetCount = () => {
+        setCount(0);
+    }
+    const decreaseCount = () => {
+        setCount(count - 1);
+    }
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={increaseCount}>Increment</button>
+      <button onClick={resetCount}>Reset</button>
+      <button onClick={decreaseCount}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter
