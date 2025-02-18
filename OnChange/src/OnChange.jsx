fb1150805d111470b84fd9850bd1b1bd4887ead1
TauqeerDevX <hasantauqeer20@gmail.com>
2025-02-18 23:35:30 +0530

    On change even with option feature

--- OnChange/src/OnChange.jsx
+++ OnChange/src/OnChange.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { useState } from 'react'
 
 const OnChange = () => {
-    const [name, setName] = useState("")
+    const [name, setName] = useState()
     const handleChange = (e) => {
         setName(e.target.value)
     }
+    const [payment, setPayment] = useState()
+    const handlePaymentChange = (e) => {
+        setPayment(e.target.value)
+    }
   return (<div>
-    <input type="text" onChange={handleChange}/>
+    <input type="text" value= {name} onChange={handleChange}/>
     <p>Name: {name}</p>
+    <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="MasterCard">MasterCard</option>
+    </select>
+    <p>Payment: {payment}</p>
     </div>
   )
 }
